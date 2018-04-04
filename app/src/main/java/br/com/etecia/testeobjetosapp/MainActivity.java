59f12bbb732a81158b4dd7bfa75cb77efa80a59d
lndsilva <lndsilva@hotmail.com>
2018-04-03 21:48:46 -0300

    versão Final

--- app/src/main/java/br/com/etecia/testeobjetosapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/testeobjetosapp/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button btnEntrar;
+    private Button btnEntrar;
+    private TextView txtNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnEntrar = (Button)findViewById(R.id.btnEntrar);
+        txtNome = (TextView) findViewById(R.id.txtNome);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String textoDigitado = "";
+                textoDigitado = txtNome.getText().toString();
                 Intent intent = new Intent(MainActivity.this,SegundoActivity.class);
+
+                Bundle bundle = new Bundle();
+
+                bundle.putString("textoDigitado",textoDigitado);
+                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
