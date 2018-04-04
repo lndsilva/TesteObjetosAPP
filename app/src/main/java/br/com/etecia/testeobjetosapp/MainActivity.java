a18217dd768e8f2757f7205e1d1b61134ce44823
lndsilva <lndsilva@hotmail.com>
2018-04-03 21:13:43 -0300

    Final

---
+++ app/src/main/java/br/com/etecia/testeobjetosapp/MainActivity.java
@@ -0,0 +1,32 @@
+package br.com.etecia.testeobjetosapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEntrar = (Button)findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SegundoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
+}
